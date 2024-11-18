309b9da3ff7496db8da8c18e4b6b589e7a4e152c
arifahnbm <nura60583@gmail.com>
2024-11-18 15:32:50 +0700

    first commit

---
+++ lib/screens/history_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/attendance_provider.dart';
+
+class HistoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<AttendanceProvider>(context);
+    var history = provider.history;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Riwayat Kehadiran')),
+      body: history.isEmpty
+          ? Center(child: Text('Belum ada riwayat'))
+          : ListView.builder(
+              itemCount: history.length,
+              itemBuilder: (context, index) {
+                var record = history[index];
+                return ListTile(
+                  title: Text(
+                      'Tanggal: ${record.date.toLocal().toString().split(' ')[0]}'),
+                  subtitle: Text(
+                      'Hadir: ${record.presentCount}, Tidak Hadir: ${record.absentCount}'),
+                );
+              },
+            ),
+    );
+  }
+}
